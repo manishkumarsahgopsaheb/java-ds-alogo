a78ee824d3c101e37a684839b7da16162a0e97e1
Manish Kumar Sah <47948821+manishkumarsahgopsaheb@users.noreply.github.com>
2021-09-09 21:25:10 +0530

    Update 02ClimbStairPathCountMem.java

--- Dynamic programming/02ClimbStairPathCountMem.java
+++ Dynamic programming/02ClimbStairPathCountMem.java
@@ -7,8 +7,11 @@ public class Main {
         
         int count = CountStairPathMemoization(n, mem);
         System.out.println(count);
+        System.out.println(pathCount(n));
     }
     
+    
+    // recursion + memoization
     public static int CountStairPathMemoization(int n, int[] mem){
         if(n == 0){
             return 1; // path from 0 to 0 is 1
@@ -28,6 +31,25 @@ public class Main {
         mem[n] = pcount;
         return pcount;
     }
+    
+    // dp solution using tabulization
+    
+    public static int pathCount(int n){
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        
+        for(int i=1;i<=n;i++){
+            if(i==1){
+                dp[i] = dp[i-1];
+            }else if(i==2){
+                dp[i] = dp[i-1] + dp[i-2];
+            }else{
+                dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+            }
+        }
+        return dp[n];
+    }
 }
-// input n = 5
-// total path count = 13
+
+// input n = 6
+// count = 24
